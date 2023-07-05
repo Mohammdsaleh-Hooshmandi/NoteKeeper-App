6524b1fc0da711b1948f853a595f64359029e94d
Mohammdsaleh Hooshmandi <Mohammdsale.Hooshmandi1382@gmail.com>
2023-07-05 20:31:16 +0430

    add real data for nots

--- src/Data.js
+++ src/Data.js
@@ -1,10 +1,8 @@
 const allNote = [
-    { id: "note-1", title: "this is the title of note", note: "this is the body of note"},
-    { id: "note-2", title: "this is the title of note", note: "this is the body of note"},
-    { id: "note-3", title: "this is the title of note", note: "this is the body of note"},
-    { id: "note-4", title: "this is the title of note", note: "this is the body of note"},
-    { id: "note-5", title: "this is the title of note", note: "this is the body of note"},
-    { id: "note-6", title: "this is the title of note", note: "this is the body of note"},
+    { id: "note-1", title: "HTML", note: "The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser. It is often assisted by technologies such as Cascading Style Sheets and scripting languages such as JavaScript"},
+    { id: "note-2", title: "CSS", note: "Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML or XML. CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript"},
+    { id: "note-3", title: "Java Script", note: "JavaScript, often abbreviated as JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS. As of 2023, 98.7% of websites use JavaScript on the client side for webpage behavior, often incorporating third-party libraries"},
+    { id: "note-4", title: "React.js", note: "React is a free and open-source front-end JavaScript library for building user interfaces based on components. It is maintained by Meta and a community of individual developers and companies. React can be used to develop single-page, mobile, or server-rendered applications with frameworks like Next.js"}
 ];
 
 export default allNote;
